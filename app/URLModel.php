bcc8d44f02f43546b6d8fbc29ca8e8d5d2deec53
Musa Ugurlu <mugurlu@outlook.com>
2016-04-16 05:18:00 +0000

    Need to add fillable fields to model

--- app/URLModel.php
+++ app/URLModel.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class URLModel extends Model
 {
-    //
+    protected $table = 'urls';
+    
+    protected $fillable = ['link','destination','enabled','expires']; //need to add more
 }
